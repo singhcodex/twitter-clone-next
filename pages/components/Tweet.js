0da9b0d57b065cf3dc7e51078a792b114c85fba7
singhcodex <singh.hessen@gmail.com>
2023-04-21 16:19:38 +0200

    images from faker show after config

--- pages/components/Tweet.js
+++ pages/components/Tweet.js
@@ -1,10 +1,21 @@
 import timeago from "@/lib/timeago";
+import Image from "next/image";
 
 export default function Tweet ({tweet}){
     return (
         <>
         
         <div className="card border m-3 rounded px-2 py-5">
+            <p>
+                {tweet.author.image && (
+                    <Image
+                    className='rounded-full'
+                    src={tweet.author.image}
+                    alt={tweet.author.name}
+                    width='40' height='40'
+                    />
+                )}
+            </p>
             <p className="float-right">
                 {timeago.format(new Date(tweet.createdAt))}
             </p>
